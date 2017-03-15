fd238bb22bebdc5a117d4485e806a9e3b898a001
Alejandro Trigo <alejandro.trigo@udc.es>
2017-03-15 23:07:18 +0100

    Segundo punto de la práctica

---
+++ p1ri/src/main/java/udc/ri/p1ri/IndexProcessing.java
@@ -0,0 +1,51 @@
+package udc.ri.p1ri;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+public class IndexProcessing {
+
+	private IndexProcessing(){}
+	
+	public static void main(String[] args){
+		//TODO validate arguments
+		//IMPORTANT Supongo que solo se puede elegir una opción por cada vez que se llama al programa
+		String usage = "-indexin indexfile " 
+						+"-best_idfterms field n "
+						+"-poor_idfterms field n "
+						+"-best_tfidfterms field n "
+						+"-poor_tfidfterms field n";
+		String indexin;
+		String field;
+		int numElements;
+	}
+}
