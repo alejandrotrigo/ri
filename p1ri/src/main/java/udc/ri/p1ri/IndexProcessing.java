5b97bd79ca74a408784ef18f20716dacaed7320b
Alejandro Trigo <alejandro.trigo@udc.es>
2017-03-19 12:05:05 +0100

    34

--- p1ri/src/main/java/udc/ri/p1ri/IndexProcessing.java
+++ p1ri/src/main/java/udc/ri/p1ri/IndexProcessing.java
@@ -46,6 +46,74 @@ public class IndexProcessing {
 
 	private IndexProcessing(){}
 	
+	public static List<Term> getNBestIdfTerms(String fieldU, int n, DirectoryReader indexReader){
+		
+		int numDocs = indexReader.maxDoc();
+		List<(double, int)> termList = new ArrayList<>();
+		
+		for (final LeafReaderContext leaf : indexReader.leaves()) {
+			// Print leaf number (starting from zero)
+			System.out.println("We are in the leaf number " + leaf.ord);
+
+			// Create an AtomicReader for each leaf
+			// (using, again, Java 7 try-with-resources syntax)
+			try (LeafReader leafReader = leaf.reader()) {
+
+				// Get the fields contained in the current segment/leaf
+				final Fields fields = leafReader.fields();
+
+				//Opción 1 y 2, best n terms by idf terms
+				System.out.println("Field = " + fieldU);
+				final Terms terms = fields.terms(fieldU);
+				final TermsEnum termsEnum = terms.iterator();
+				int docFreq = 0;
+				while (termsEnum.next() != null) {
+					final String tt = termsEnum.term().utf8ToString();
+					double idf = Math.log(numDocs/termsEnum.totalTermFreq());
+					termList.add(idf);
+
+				}
+				
+			}catch(IOException e){
+				System.out.println(e);
+			}	
+		}
+		return termList;
+	}
+	
+public static String getNBestTfIdfTerms(String fieldU, int n, DirectoryReader indexReader){
+		
+		int numDocs = indexReader.maxDoc();
+		
+		for (final LeafReaderContext leaf : indexReader.leaves()) {
+			// Print leaf number (starting from zero)
+			System.out.println("We are in the leaf number " + leaf.ord);
+
+			// Create an AtomicReader for each leaf
+			// (using, again, Java 7 try-with-resources syntax)
+			try (LeafReader leafReader = leaf.reader()) {
+
+				// Get the fields contained in the current segment/leaf
+				final Fields fields = leafReader.fields();
+
+				//Opción 1 y 2, best n terms by idf terms
+				System.out.println("Field = " + fieldU);
+				final Terms terms = fields.terms(fieldU);
+				final TermsEnum termsEnum = terms.iterator();
+				int docFreq = 0;
+				while (termsEnum.next() != null) {
+					final String tt = termsEnum.term().utf8ToString();
+					double idf = Math.log(numDocs/termsEnum.totalTermFreq());
+					docFreq = termsEnum.docFreq();
+				}
+				
+			}catch(IOException e){
+				System.out.println(e);
+			}	
+		}
+		return "TODO ";
+	}
+	
 	public static void main(String[] args){
 		//TODO validate arguments
 		//IMPORTANT Supongo que solo se puede elegir una opción por cada vez que se llama al programa
@@ -56,7 +124,7 @@ public class IndexProcessing {
 						+"-poor_tfidfterms field n";
 		String indexin = null;
 		String fieldU = null;
-		int numElements;
+		int numElements = 0;
 		
 		for(int i=0; i<args.length;i++){
 			if("-indexin".equals(args[i])){
@@ -85,12 +153,14 @@ public class IndexProcessing {
 
 		Directory dir = null;
 		DirectoryReader indexReader = null;
+		DirectoryReader indexReader2 = null;
 
 		//List<IndexableField> fields = null;
 		
 		try {
 			dir = FSDirectory.open(Paths.get(indexFolder));
 			indexReader = DirectoryReader.open(dir);
+			indexReader2 = DirectoryReader.open(dir);
 		} catch (CorruptIndexException e1) {
 			System.out.println("Graceful message: exception " + e1);
 			e1.printStackTrace();
@@ -104,66 +174,9 @@ public class IndexProcessing {
 		
 		
 		//Opciones 3 y 4 
-		/*try{
-			for (int docNum=0; docNum<numDocs; docNum++){
-				Terms termVector = indexReader.getTermVector(docNum, fieldU);
-				System.out.println("DOCNUM: " + docNum + " field: " + fieldU);
-				TermsEnum itr = termVector.iterator();
-				BytesRef term = null;
-				PostingsEnum postings = null;
-				double frecuencia = 0;
-				while((term = itr.next()) != null){
-					try{
-						String termText = term.utf8ToString();
-						postings = itr.postings(postings, PostingsEnum.FREQS);
-						int freq = postings.freq();
-						if (freq < 1){
-							frecuencia = 0;
-						}else{
-							frecuencia = 1 + Math.log(freq);
-						}
-						System.out.println("doc:" + docNum + ", term: " + termText + ", tfIdf = " + frecuencia);
-					} catch(Exception e){
-						System.out.println(e);
-					}
-				
-				}
-			}
-			
-		}catch(IOException e){
-			System.out.println(e);
-		}*/
+		String s = getNBestIdfTerms(fieldU, numElements, indexReader);
+		//System.out.println("Size of  indexReader.leaves() = " + indexReader.leaves().size());
 		
-		System.out.println("Size of  indexReader.leaves() = " + indexReader.leaves().size());
-		for (final LeafReaderContext leaf : indexReader.leaves()) {
-			// Print leaf number (starting from zero)
-			System.out.println("We are in the leaf number " + leaf.ord);
-
-			// Create an AtomicReader for each leaf
-			// (using, again, Java 7 try-with-resources syntax)
-			try (LeafReader leafReader = leaf.reader()) {
-
-				// Get the fields contained in the current segment/leaf
-				final Fields fields = leafReader.fields();
-				System.out.println("Numero de campos devuelto por leafReader.fields() = " + fields.size());
-
-				//Opción 1 y 2, best n terms by idf terms
-				System.out.println("Field = " + fieldU);
-				final Terms terms = fields.terms(fieldU);
-				final TermsEnum termsEnum = terms.iterator();
-				while (termsEnum.next() != null) {
-					final String tt = termsEnum.term().utf8ToString();
-					double idf = Math.log(numDocs/termsEnum.totalTermFreq());
-					//System.out.println("Especificidad del término: " + tt + " : " + idf + " número de documentos: " + numDocs + " frecuencia del término: " + termsEnum.totalTermFreq());
-				}
-				
-				
-			}catch(IOException e){
-				System.out.println(e);
-			}
-			
-			
-		}
 		
 	}
 }
